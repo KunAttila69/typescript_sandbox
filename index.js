9b0b0b5f7b932fb4596b1043244d5d430e05d2fb
KunAttila69 <klun.attila0508@gmail.com>
2024-09-04 10:11:37 +0200

    Number guesser

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 "use strict";
-let greet = "Hello TypeScript!";
-console.log(greet);
+const numEl = document.getElementById("inputNum");
+const resultEl = document.getElementById("result");
+const theNumber = Math.floor(Math.random() * 100) + 1;
+const Guess = () => {
+    const guessNumber = parseInt(numEl.value);
+    if (IsCorrect(guessNumber)) {
+        resultEl.innerText = "Eltaláltad";
+    }
+    else {
+        if (guessNumber < theNumber) {
+            resultEl.innerText = "A gondolt szám nagyobb!";
+        }
+        else {
+            resultEl.innerText = "A gondolt szám kisebb!";
+        }
+    }
+};
+const IsCorrect = (guessNumber) => {
+    return guessNumber === theNumber;
+};
--- index.ts
+++ index.ts
@@ -1,2 +1,25 @@
-let greet: string = "Hello TypeScript!"
-console.log(greet)
+const numEl: HTMLInputElement = document.getElementById("inputNum") as HTMLInputElement
+
+const resultEl: HTMLParagraphElement = document.getElementById("result") as HTMLParagraphElement
+
+const theNumber: number = Math.floor(Math.random() * 100) + 1
+
+const Guess = () => {
+    const guessNumber: number = parseInt(numEl.value)
+    
+    if (IsCorrect(guessNumber)) {
+        resultEl.innerText = "Eltaláltad"
+    }
+    else{
+        if(guessNumber < theNumber){
+            resultEl.innerText = "A gondolt szám nagyobb!"
+        }
+        else{
+            resultEl.innerText = "A gondolt szám kisebb!"
+        }
+    }
+}
+
+const IsCorrect = (guessNumber: number) : boolean => {
+    return guessNumber === theNumber
+}
